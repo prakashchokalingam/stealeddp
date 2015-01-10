338958fc7d7804504d861d3bbb0c75202dd98afe
prakashchokalingam <cpvicky33@gmail.com>
2015-01-10 11:08:15 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,7 +1,7 @@
 <?php
 $code=$_GET['code'];
-$appid='192892070896104';
-$appsec='3bd463481c4e15995e4b235e6cebc83a';
+$appid='xxxxxxxxxxxxxxxxxxxxx';
+$appsec='xxxxxxxxxxxxxxxxxxxxx';
 $re='https://stealeddp.herokuapp.com/login.php';
 $a='https://graph.facebook.com/oauth/access_token?code='.$code.'&client_id='.$appid.'&client_secret='.$appsec.'&redirect_uri='.$re;
 $x=file_get_contents($a);
